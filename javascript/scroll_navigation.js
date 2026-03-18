39a7ea49918dbea8ea97224ab509b8cb0bfc9e75
rahulc <rahulchitkara2001@gmail.com>
2026-03-18 13:42:11 +1100

    made the scroll-to-top-button only appear when the user scrolls past half the page

--- javascript/scroll_navigation.js
+++ javascript/scroll_navigation.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const navigationButtons = document.getElementById('main-navigation-buttons').children;
     const sections = document.getElementsByClassName('section');
 
+    // Scroll Navigation Bar
     for (let button of navigationButtons) {
 
         button.addEventListener('click', (button) => {
@@ -16,8 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Scroll to Top Button
     const scrollToTopButton = document.getElementById('scroll-to-top-button');
     scrollToTopButton.addEventListener('click', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     })
+
+    document.addEventListener('scroll', () => {
+        if (window.scrollY >= (document.documentElement.scrollHeight / 2)) {
+            scrollToTopButton.classList.add('scroll-to-top-button-animation');
+        } else {
+            scrollToTopButton.classList.remove('scroll-to-top-button-animation');
+        }
+    });
 });
